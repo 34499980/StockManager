2fbbfde639e688c7199c57fdf6c7a7ca077757ca
NOTEBOOKDELL\Dell <aribrenman@hotmail.com>
2020-12-27 14:25:27 -0300

    design userlist

--- src/app/pages/profiles/userlist/userlist.component.ts
+++ src/app/pages/profiles/userlist/userlist.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { ArquitecturaService } from 'src/app/services/arquitectura.service';
 import { User, UserGet } from 'src/app/models/user';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatDrawer, MatSidenav } from '@angular/material/sidenav';
 import { Item } from 'src/app/models/item.model';
@@ -13,6 +13,7 @@ import { UserFilter } from 'src/app/models/UserFilter.model';
 import { ValueCache } from 'ag-grid-community';
 import { AuthenticationService } from 'src/app/core/services/authentication.service';
 import { RolesEnum } from 'src/app/enums/Roles.Enum';
+import { AppRouting } from 'src/app/enums/AppRouting.enum';
 
 
 @Component({
@@ -34,7 +35,8 @@ export class UserListComponent implements OnInit {
   constructor(private activateRoute: ActivatedRoute,
               private formBuilder: FormBuilder,
               private userService: UserService,
-              private authenticationService: AuthenticationService) {
+              private authenticationService: AuthenticationService,
+              private router: Router) {
 
    }
 
@@ -62,6 +64,9 @@ export class UserListComponent implements OnInit {
      }
       
    });
+  }
+  Add(){
+    this.router.navigate([AppRouting.Profile])
   }
   getUsersFilter(){
 
