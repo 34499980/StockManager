d08b8e77b2d8112b9584d3db1a61addc24ded9c4
NOTEBOOKDELL\Dell <aribrenman@hotmail.com>
2021-01-05 18:32:59 -0300

    ErrorHandler

--- src/app/interceptors/error/errorInterceptor.ts
+++ src/app/interceptors/error/errorInterceptor.ts
@@ -1,5 +1,6 @@
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { ArquitecturaService } from 'src/app/services/arquitectura.service';
@@ -7,14 +8,15 @@ import { ArquitecturaService } from 'src/app/services/arquitectura.service';
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
     constructor(
-        private artuitecturaService: ArquitecturaService
+        private artuitecturaService: ArquitecturaService,
+        private translate: TranslateService,
       ) {
       }
   intercept<T>(req: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<T>>{
     return next.handle(req)
       .pipe(
-        catchError((error: HttpErrorResponse) => {
-            this.artuitecturaService.openDialog('Error', error.error)
+        catchError((error: HttpErrorResponse) => {          
+            this.artuitecturaService.openDialog('Error', this.translate.instant( 'ERRORS.' + error.error))
           return throwError(error.status);
         })
       )
